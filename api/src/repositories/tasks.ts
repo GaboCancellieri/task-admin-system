3229b58bf705c135070fdc3da1b632113b8fff9f
Gabo <cancellieri.gabriel@gmail.com>
2021-12-15 09:48:21 -0300

    ✨ Add findWithLimit, insertMany and findOneAndUpdate repository func

--- api/src/repositories/tasks.ts
+++ api/src/repositories/tasks.ts
@@ -27,11 +27,57 @@ export async function findAll() {
     }
     return result;
   } catch (error) {
-      console.error("Error in findAll tasks from DB");
+      console.error("Error while finding all tasks from DB");
       console.error(error);
       throw new BasicError("Internal Server Error", 500);
   }
 }
+
+/**
+ * Find with a limited amount of tasks from the DB
+ * @returns {Task[]} - A array of tasks
+ * @throws Will throw an error if any problem ocurres with the DB
+ *
+ */
+ export async function findWithLimit(limit: number): Promise<Task[]> {
+  try {
+    let result: any[] = [];
+    const collectionTasks = getTasksCollection();
+    if (collectionTasks) {
+      result = await collectionTasks.find().limit(limit).toArray();
+    }
+    return result;
+  } catch (error) {
+      console.error("Error while finding some tasks from DB");
+      console.error(error);
+      throw new BasicError("Internal Server Error", 500);
+  }
+}
+
+/**
+ * Insert a Array of Tasks into the DB
+ * @param {Task[]} [tasks] - Array of tasks
+ * @returns {Task[]} - A array of tasks with _id
+ * @throws Will throw an error if any problem ocurres with the DB
+ *
+ */
+ export async function insertMany(tasks: Task[]): Promise<Task[] | undefined> {
+  try {
+    let result;
+    const collectionTasks = getTasksCollection();
+    if (collectionTasks) {
+      const insertResult = await collectionTasks.insertMany(tasks);
+      //to return the inserted objects (_id,name,show)
+      // const insertedObjects = insertResult.ops;
+      result = tasks;
+    }
+    return result;
+  } catch (error) {
+    console.error("Error while inserting the tasks to the DB");
+    console.error(error);
+    throw new BasicError("Internal Server Error", 500);
+  }
+};
 
 /**
  * Insert a Task in the DB
@@ -44,13 +90,41 @@ export async function findAll() {
   try {
     const collectionTasks = getTasksCollection();
     if (collectionTasks) {
-      const insertResult = await collectionTasks.insertOne(task);
-      console.log(insertResult);
-      // const taskNewObject = insertResult.ops[0];
+      await collectionTasks.insertOne(task);
       return task;
     }
   } catch (error) {
     console.error("Error when insert a task to the DB");
+    console.error(error);
+    throw new BasicError("Internal Server Error", 500);
+  }
+}
+
+/**
+ * Find a task and update it
+ * @param {object} [filters] - The filters for the find
+ * @param {Task} [newTask] - The object that will replace the current task
+ * @returns {Task} - The new task object
+ * @throws Will throw an error if any problem ocurres with the DB
+ *
+ */
+ export async function findOneAndUpdate(filters: { id: string }, newTask: Task) {
+  try {
+    const collectionTasks = getTasksCollection();
+    if (collectionTasks) {
+      const result = await collectionTasks.findOneAndUpdate(
+        filters,
+        {
+          $set: newTask,
+        },
+        { returnDocument: 'after' } // return the updated object
+      );
+  
+      const { value: updatedTask } = result; // If its updated, returns the new object. If there no exist the target, returns null
+      return updatedTask;
+    }
+  } catch (error) {
+    console.error("Error in the findOneAndUpdate of tasks repository");
     console.error(error);
     throw new BasicError("Internal Server Error", 500);
   }
